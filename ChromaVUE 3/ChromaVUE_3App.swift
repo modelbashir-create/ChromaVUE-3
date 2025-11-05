e6295813af6f7239cae4564ef55e9e1662a09ebd
Mohamed <modelbashir@gmail.com>
2025-11-04 20:56:21 -0500

    Initial Commit

---
+++ ChromaVUE 3/ChromaVUE_3App.swift
@@ -0,0 +1,17 @@
+//
+//  ChromaVUE_3App.swift
+//  ChromaVUE 3
+//
+//  Created by Mohamed Elbashir on 11/4/25.
+//
+
+import SwiftUI
+
+@main
+struct ChromaVUE_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
